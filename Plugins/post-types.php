ace75995cba77ec41121db7ae0ad6bb1e7033d3e
dewshan-sarathchandra <sarathcd@sheridancollege.ca>
2016-03-21 01:28:55 -0400

    Create post-types.php

---
+++ Plugins/post-types.php
@@ -0,0 +1,10 @@
+<?php 
+/*Plugin Name: Create Product Post Type
+Description: This plugin registers a custom post type for featured products.
+License: GPLv2
+Authors: Dewshan Sarathchandra, 
+*/
+
+/* ---------------------------------
+			CUSTOM POST TYPE
+---------------------------------- */
