85b0f9d801dfaefdd7cd1961f6a7c3358c3a499f
dewshan-sarathchandra <sarathcd@sheridancollege.ca>
2016-03-22 01:07:41 -0400

    Create latest-custom-posts.php

---
+++ inc/latest-custom-posts.php
@@ -0,0 +1,9 @@
+<?php
+/*Plugin Name: Widget to display latest custom posts 
+Description: This plugin registers a widget to display latest custom posts
+Authors: Dewshan Sarathchandra, Amrita Singh, Kariann Brown 
+*/
+
+/* ------------------------------------------------------
+Register the 'Latest Custom Posts' widget
+-------------------------------------------------------*/
